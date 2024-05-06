4c6a8fe07485bf1f52ecdfd3df04013cec9b451e
Sohal Sudheer <sohal@sohalsdr.com>
2024-05-06 18:20:37 -0400

    Update home.nix

--- taipei/human/home.nix
+++ taipei/human/home.nix
@@ -8,28 +8,12 @@
   ...
 }: {
   # You can import other home-manager modules here
-  imports = [
-    # If you want to use home-manager modules from other flakes (such as nix-colors):
-    # inputs.nix-colors.homeManagerModule
-
-    # You can also split up your configuration and import pieces of it here:
-    # ./nvim.nix
-  ];
+  imports = [];
 
   nixpkgs = {
     # You can add overlays here
-    overlays = [
-      # If you want to use overlays exported from other flakes:
-      # neovim-nightly-overlay.overlays.default
-
-      # Or define it inline, for example:
-      # (final: prev: {
-      #   hi = final.hello.overrideAttrs (oldAttrs: {
-      #     patches = [ ./change-hello-to-hi.patch ];
-      #   });
-      # })
-    ];
-    # Configure your nixpkgs instance
+    overlays = [];
+    # Configure nixpkgs instance
     config = {
       # Disable if you don't want unfree packages
       allowUnfree = true;
@@ -38,15 +22,14 @@
     };
   };
 
-  # TODO: Set your username
   home = {
     username = "human";
     homeDirectory = "/home/human";
   };
 
-  # Add stuff for your user as you see fit:
-  # programs.neovim.enable = true;
-  # home.packages = with pkgs; [ steam ];
+  home.packages = with pkgs; [ 
+      firefox
+  ];
 
   # Enable home-manager and git
   programs.home-manager.enable = true;
